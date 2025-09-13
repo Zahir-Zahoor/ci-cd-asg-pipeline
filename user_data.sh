56c00a2c0c83842f08f3aa500ba9fd0ace415253
Zahir-Zahoor <zahirzahoor63@gmail.com>
2025-09-13 12:04:39 +0530

    new cuser_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-sudo su
+
+# Update system
 dnf update -y
 dnf upgrade -y
-dnf install nginx -y
+
+# Install Nginx
+dnf install -y nginx
 systemctl enable nginx
 systemctl start nginx
-dnf install ruby -y
-dnf install wget -y
+
+# Install Ruby & Wget (needed for CodeDeploy Agent)
+dnf install -y ruby wget
+
+# Install AWS CodeDeploy Agent
 cd /home/ec2-user
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 ./install auto
+
+# Enable and start CodeDeploy Agent service
 systemctl enable codedeploy-agent
 systemctl start codedeploy-agent
